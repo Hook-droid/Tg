e926578b164c85e7a3f4f1755879fa9a56b1a7e0
Hook-droid <lyroxhook@gmail.com>
2026-08-18 01:19:47 +0300

    Add files via upload

--- TMessagesProj/src/main/java/org/telegram/messenger/LyrxWhisper.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LyrxWhisper.java
@@ -1,6 +1,10 @@
 package org.telegram.messenger;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LyrxWhisper {
 
@@ -89,6 +93,89 @@ public class LyrxWhisper {
             }
             contextPtr = 0;
         }
+    }
+
+    public interface DownloadCallback {
+        void onProgress(int percent);
+        void onDone(boolean success);
+    }
+
+    private static volatile boolean downloading;
+
+    public static boolean isDownloading() {
+        return downloading;
+    }
+
+    public static void downloadModel(DownloadCallback callback) {
+        if (downloading) {
+            return;
+        }
+        downloading = true;
+        new Thread(() -> {
+            boolean ok = false;
+            File target = getModelFile();
+            File temp = new File(target.getAbsolutePath() + ".part");
+            HttpURLConnection conn = null;
+            InputStream in = null;
+            FileOutputStream fos = null;
+            try {
+                if (temp.exists()) {
+                    temp.delete();
+                }
+                URL url = new URL(MODEL_URL);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setConnectTimeout(15000);
+                conn.setReadTimeout(30000);
+                conn.setInstanceFollowRedirects(true);
+                conn.connect();
+                int total = conn.getContentLength();
+                in = conn.getInputStream();
+                fos = new FileOutputStream(temp);
+                byte[] buffer = new byte[65536];
+                long written = 0;
+                int lastPercent = -1;
+                int read;
+                while ((read = in.read(buffer)) > 0) {
+                    fos.write(buffer, 0, read);
+                    written += read;
+                    if (total > 0 && callback != null) {
+                        int percent = (int) (written * 100 / total);
+                        if (percent != lastPercent) {
+                            lastPercent = percent;
+                            final int p = percent;
+                            AndroidUtilities.runOnUIThread(() -> callback.onProgress(p));
+                        }
+                    }
+                }
+                fos.flush();
+                fos.close();
+                fos = null;
+                if (written > 1024 * 1024) {
+                    if (target.exists()) {
+                        target.delete();
+                    }
+                    ok = temp.renameTo(target);
+                }
+            } catch (Throwable e) {
+                FileLog.e(e);
+            } finally {
+                try { if (fos != null) fos.close(); } catch (Throwable ignore) {}
+                try { if (in != null) in.close(); } catch (Throwable ignore) {}
+                try { if (conn != null) conn.disconnect(); } catch (Throwable ignore) {}
+                if (temp.exists()) {
+                    temp.delete();
+                }
+            }
+            downloading = false;
+            final boolean result = ok;
+            if (callback != null) {
+                AndroidUtilities.runOnUIThread(() -> callback.onDone(result));
+            }
+        }).start();
+    }
+
+    public static boolean isAvailable() {
+        return SharedConfig.lyrxWhisperEnabled && isModelReady() && loadLibrary();
     }
 
     public static synchronized String transcribe(float[] samples, String language) {
